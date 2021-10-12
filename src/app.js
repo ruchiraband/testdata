ed70056f422288c69793a2ba678c86aba14055c4
Ruchiraband <ruchira.band@techinfini.com>
2021-10-12 12:34:51 +0530

    connection string update

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("./db/conn");
 const Student = require("./models/students");
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
+const url = "mongodb+srv://ruchiraband:ruchira$273@cluster0.hv7nn.mongodb.net/student-api?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb://localhost:27017/student-api", {
+mongoose.connect(url, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
     console.log("connection is successful.");
 }).catch(() => {
-    console.log("No Connection.")
+    console.log("No Connection.");
 });
